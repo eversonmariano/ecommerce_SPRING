32cd0ac7dfae8b8714b56663c5285f3760b0ee4f
Everson Mariano <mariano.computacao@gmail.com>
2024-05-28 16:46:03 -0300

    proj configurado + Entidade MarcaProduto criada

---
+++ e_commerce/src/main/java/com/mariano/ecommerce/model/MarcaProduto.java
@@ -0,0 +1,38 @@
+package com.mariano.ecommerce.model;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "marca_produto")
+@SequenceGenerator(name = "seq_marca_produto", sequenceName = "seq_marca_produto", allocationSize = 1, initialValue = 1)
+public class MarcaProduto implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_marca_produto")
+    private Long id;
+
+    private String nomeDesc;
+
+
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomeDesc() {
+        return nomeDesc;
+    }
+
+    public void setNomeDesc(String nomeDesc) {
+        this.nomeDesc = nomeDesc;
+    }
+}
